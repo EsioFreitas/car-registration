d77b6312ee92445241774d35de8e3c9b9ed76a2a
EsioFreitas <esio.gustavo@gmail.com>
2019-03-14 09:24:17 -0300

    Add input helper

---
+++ src/helpers/inputs.js
@@ -0,0 +1,76 @@
+const inputs = {
+    placa: {
+        label: "Placa",
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: ''
+        },
+        value: '',
+    },
+    marca: {
+        label: "Marca",
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: ''
+        },
+        value: '',
+    },
+    modelo: {
+        label: "Modelo",
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: ''
+        },
+        value: '',
+    },
+    ano: {
+        label: "Ano",
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: ''
+        },
+        value: '',
+    },
+    cor: {
+        label: "Cor",
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: ''
+        },
+        value: '',
+    },
+    revisao: {
+        label: "Revisão",
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: ''
+        },
+        value: '',
+    },
+    chassi: {
+        label: "Chassi",
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: ''
+        },
+        value: '',
+    },
+    obs: {
+        label: "Observações",
+        type: 'input',
+        config: {
+            type: 'text',
+            placeholder: ''
+        },
+        value: '',
+    }
+}
+
+export default inputs
