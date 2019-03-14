84a9e181745c47189c232ee7c29466655530de10
EsioFreitas <esio.gustavo@gmail.com>
2019-03-14 09:27:49 -0300

    Add DB helper

---
+++ src/helpers/DB.js
@@ -0,0 +1,40 @@
+const DB =[
+    {
+        id: 1,
+        car: {
+            placa: '',
+            modelo: '',
+            ano: '',
+            cor: '',
+            revisao: '',
+            chassi: '',
+            obs: '',
+        }
+    },
+    {
+        id: 2,
+        car: {
+            placa: '',
+            modelo: '',
+            ano: '',
+            cor: '',
+            revisao: '',
+            chassi: '',
+            obs: '',
+        }
+    },
+    {
+        id: 3,
+        car: {
+            placa: '',
+            modelo: '',
+            ano: '',
+            cor: '',
+            revisao: '',
+            chassi: '',
+            obs: '',
+        }
+    }
+]
+
+export default DB;
