9ff7358f879d8c87b1f853412667ac90a80d2320
EsioFreitas <esio.gustavo@gmail.com>
2019-03-14 09:07:21 -0300

    Add homePage container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
+import HomePage from './container/homePage'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <HomePage/>
       </div>
     );
   }
---
+++ src/container/homePage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import './homePage.scss';
+
+
+class HomePage extends Component{
+    render(){
+        return(
+            <div>
+                oi
+            </div>
+        )
+    }
+}
+
+export default HomePage;
