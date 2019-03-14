eed2b9b71e1c4cdc08ec3fc41d250f16a4b4b5b3
EsioFreitas <esio.gustavo@gmail.com>
2019-03-14 18:14:51 -0300

    Updates

---
+++ precache-manifest.1c1d8c37bc2425d157155ea70d679893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58f332a7d5a32ca89485",
+    "url": "/car-registration/static/css/main.617ef75e.chunk.css"
+  },
+  {
+    "revision": "58f332a7d5a32ca89485",
+    "url": "/car-registration/static/js/main.8d73540c.chunk.js"
+  },
+  {
+    "revision": "b7472ac99c550179bcb4",
+    "url": "/car-registration/static/js/runtime~main.ca8c4467.js"
+  },
+  {
+    "revision": "e640c033c78b66550380",
+    "url": "/car-registration/static/js/2.a2b1cf27.chunk.js"
+  },
+  {
+    "revision": "14e6a6886dff0a6d6a9e5abc3aa1abfa",
+    "url": "/car-registration/index.html"
+  }
+];
