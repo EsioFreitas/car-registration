7ec020c69b0b3f297f4444e76f8a42f51f3dccad
EsioFreitas <esio.gustavo@gmail.com>
2019-03-16 18:07:34 -0300

    Add modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 import './Modal.scss'
 
 const modal = props => (
-    <div>
-        
-    </div>
+    <Fragment>
+        <div className='modal'
+             style={{
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: this.props.show ? '1' : '0'
+             }}>
+            {props.children}
+        </div>
+    </Fragment>
 );
 
 export default modal;
