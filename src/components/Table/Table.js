e919b89e484e42cd0d64185db8b5ed878075413e
EsioFreitas <esio.gustavo@gmail.com>
2019-03-17 13:12:52 -0300

    Dispaly sort alphabetic table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,25 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { CSVLink } from "react-csv";
 
 import './Table.scss';
 
 const table = props => {
+    let dataTable = []
+    props.db.forEach(element => {
+        console.log(element.car['marca'])
+        let dataEl = {
+            dono: element.car['dono'].nome,
+            cpf: element.car['dono'].cpf,
+            marca: element.car['marca'],
+            modelo: element.car['modelo'],
+            ano: element.car['ano'],
+            renavam: element.car['renavam'],
+            placa: element.car['placa'],
+            cor: element.car['cor'],
+        };
+
+        dataTable.push(dataEl);
+    });
 
     return (
         <div className='table-section'>
@@ -12,27 +28,34 @@ const table = props => {
                 Cadastrar veículo
             </div>
             <div className='table-section__table'>
+
                 <table >
                     <caption>Carros cadastrados</caption>
+
                     <thead className='table-section__thead'>
                         <tr>
-                            <td id='marca'>Marca</td>
-                            <td id='modelo'>Modelo</td>
-                            <td id='ano'>Ano</td>
-                            <td id='renavam'>Renavam</td>
-                            <td id='placa'>Placa</td>
-                            <td id='cor'>Cor</td>
-                            <td id='obs'>Observações</td>
+                            <td>Proprietário</td>
+                            <td>CPF</td>
+                            <td>Marca</td>
+                            <td>Modelo</td>
+                            <td>Ano</td>
+                            <td>Renavam</td>
+                            <td>Placa</td>
+                            <td>Cor</td>
                         </tr>
                     </thead>
                     <tbody className='table-section__tbody'>
                         {
-                            props.db.map((db, idxC) => (
-                                <tr key={db.id}>
-                                    {Object.keys(db.car).map((carTd, idx) => (
-                                        <td key={idx}>{props.db[idxC].car[carTd]}</td>
-                                    ))}
-                                </tr>
+                            dataTable.sort( (a, b) => {
+                                return  a.dono < b.dono ? -1 : a.dono > b.dono ? 1 : 0
+                            }).map((db, idx )=> (
+                            <tr  key={idx}>
+                                {Object.keys(db).map( el => (
+                                    <td key={el}> 
+                                        {db[el]}
+                                    </td>
+                                ))}
+                            </tr>
                             ))
                         }
                     </tbody>
@@ -48,4 +71,5 @@ const table = props => {
     );
 }
 
+//{props.db[idxC].car[carTd]}
 export default table;
